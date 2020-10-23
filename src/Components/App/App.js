a555d237401cd88844c936386593aff408d07ab5
Stasys Meclazcke <aeip@live.com>
2020-10-23 11:48:34 -0500

    imported components from folders to app

---
+++ src/Components/App/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css';
+//Components
+import Character from '../Character/Character'
+import Interactions from '../Interactions/Interactions'
+import Inventory from '../Inventory/Inventory'
+import Rooms from '../Rooms/Rooms'
+import ThreeD from '../ThreeD/ThreeD'
+//Result Components
+import Title from '../Results/Title'
+import Win from '../Results/Win'
+import Death from '../Results/Death'
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
