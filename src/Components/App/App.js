db54a603e92163f031bbaecf384b965e0ebc708a
Stasys Meclazcke <aeip@live.com>
2020-10-23 12:12:20 -0500

    appjs

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,18 +2,10 @@
 import React, {useState, useEffect} from 'react'
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-//Components
+//Game Component
 import Game from '../Game/Game'
-import Character from '../Character/Character'
-import Interactions from '../Interactions/Interactions'
-import Inventory from '../Inventory/Inventory'
-import Rooms from '../Rooms/Rooms'
-import ThreeD from '../ThreeD/ThreeD'
-//Result Components
+//Result Component
 import Titles from '../Titles/Titles'
-// import Start from '../Results/Start'
-// import Win from '../Results/Win'
-// import Death from '../Results/Death'
 
 function App() {
   return (
