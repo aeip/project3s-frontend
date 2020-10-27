5d83208e146e3eda8646e8f04170cd05c49a452a
Zane <zanebgriffin@gmail.com>
2020-10-27 14:42:52 -0500

    inventory setup

--- src/Components/Inventory/Inventory.js
+++ src/Components/Inventory/Inventory.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import {DropdownButton} from 'react-bootstrap'
+import {Dropdown} from 'react-bootstrap'
 
 export const Inventory = (props) => {
+
+	
+
 	return (
 		<div className='inventory'>
-			<button>Inventory</button>
+			<DropdownButton id="dropdown-basic-button" title="Inventory">
+				{props.character.inventory !== null ? props.character.inventory.map((item) => {
+														return(
+															<>
+															<Dropdown.Item >{item}</Dropdown.Item>
+															</>
+														)
+				}) : ''}
+  				<Dropdown.Item >Action</Dropdown.Item>
+  				<Dropdown.Item >Another action</Dropdown.Item>
+  				<Dropdown.Item >Something else</Dropdown.Item>
+			</DropdownButton>
 		</div>
 	);
 };
