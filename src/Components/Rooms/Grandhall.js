5dc3d88bb031d35bd27b122a6186e6fa3a255681
Woodrow Thompson <woodrow.thompson.iii@gmail.com>
2020-10-28 12:23:20 -0400

    stylin n stylin

--- src/Components/Rooms/Grandhall.js
+++ src/Components/Rooms/Grandhall.js
@@ -8,7 +8,20 @@ export const Grandhall = (prop) => {
 	return (
 		<div className='grandhall'>
 			<h1>Grandhall</h1>
-			<button onClick={() => nextRoom()}>Next Room</button>
+			<div className='game-screen'>
+				<div className='background'>
+					<div className='graphic'>
+						<img src='https://i.imgur.com/pNOztsH.png' />
+					</div>
+				</div>
+				<div className='character-pane'>
+
+				</div>
+				<div className='text-box'>
+					<p>You enter the foyer of the grand estate. A lone beam of moonlight shines down to the center of the room.</p> <p>Within it's pale glow is the flash of metal from a steel knife. Looks like someone dropped this in a hurry.</p>
+					<button onClick={() => nextRoom()}>Next Room</button>
+				</div>
+			</div>
 		</div>
 	);
 };
