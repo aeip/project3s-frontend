3401a844099def16df5c8540c7d240627fcebd40
Stasys Meclazcke <aeip@live.com>
2020-10-28 10:35:05 -0500

    only one knife

--- src/Components/Rooms/Foyer.js
+++ src/Components/Rooms/Foyer.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import '../Styles/Rooms/Foyer.scss'
-import '../Styles/UserInterface/GameScreen.scss'
+import '../Styles/Rooms/Foyer.scss';
+import '../Styles/UserInterface/GameScreen.scss';
 
 export const Foyer = (prop) => {
 	let props = prop.props;
@@ -8,29 +8,37 @@ export const Foyer = (prop) => {
 	const nextRoom = () => {
 		props.history.push('/game/grandhall/');
 	};
+	if (!hasKnife) {
+	}
 	const pickUpKnife = () => {
-        props.currentCharacter.inventory.push('knife');
-        props.handleUpdateCharacter(props.currentCharacter);
+		if (!hasKnife) {
+			props.currentCharacter.inventory.push('knife');
+			props.handleUpdateCharacter(props.currentCharacter);
+			setHasKnife(true);
+		}
+		props.currentCharacter.inventory.push('knife');
+		props.handleUpdateCharacter(props.currentCharacter);
 		setHasKnife(true);
 	};
 	return (
 		<div className='foyer'>
 			<h1>Foyer</h1>
-			
-			<div className='game-screen'>
 
+			<div className='game-screen'>
 				<div className='background'>
 					<div className='graphic'>
-						<img src='https://i.imgur.com/pNOztsH.png'/>
+						<img src='https://i.imgur.com/pNOztsH.png' />
 					</div>
 				</div>
 				<div className='text-box'>
-							{hasKnife ? (
-								<p>You picked up a knife</p>
-							) : (
-								<p>Would you like to pick up a knife?</p>
-							)}
-					<button onClick={() => pickUpKnife()}>Pick up knife</button>
+					{hasKnife ? (
+						<p>You picked up a knife</p>
+					) : (
+                        <>
+						<p>Would you like to pick up a knife?</p>
+                        <button onClick={() => pickUpKnife()}>Pick up knife</button>
+                        </>
+					)}
 					<button onClick={() => nextRoom()}>Next Room</button>
 				</div>
 			</div>
