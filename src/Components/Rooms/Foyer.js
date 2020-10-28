9bcf2c2f14484fe3daaa0a82a6d35aa6b5120cf8
Woodrow Thompson <woodrow.thompson.iii@gmail.com>
2020-10-28 11:45:52 -0400

    more interface edits

--- src/Components/Rooms/Foyer.js
+++ src/Components/Rooms/Foyer.js
@@ -24,7 +24,11 @@ export const Foyer = (prop) => {
 						<img src='https://i.imgur.com/pNOztsH.png'/>
 					</div>
 				</div>
+				<div className='character-pane'>
+
+				</div>
 				<div className='text-box'>
+					<p>You enter the foyer of the grand estate. A lone beam of moonlight shines down to the center of the room.</p> <p>Within it's pale glow is the flash of metal from a steel knife. Looks like someone dropped this in a hurry.</p>
 							{hasKnife ? (
 								<p>You picked up a knife</p>
 							) : (
