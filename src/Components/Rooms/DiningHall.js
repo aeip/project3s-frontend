a36636e180f95635077fcc067a6e1169d8552068
Zane <zanebgriffin@gmail.com>
2020-10-28 15:07:14 -0500

    setup the new rooms basic structure

---
+++ src/Components/Rooms/DiningHall.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DiningHall = () => {
+    return(
+        <></>
+    )
+}
+
+export default DiningHall
---
+++ src/Components/Rooms/Kitchen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Kitchen = () => {
+    return(
+        <></>
+    )
+}
+
+export default Kitchen
