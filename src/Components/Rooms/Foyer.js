caa518da795ec9bb6b9596cb3c226e68c0b2ca6b
Woodrow Thompson <woodrow.thompson.iii@gmail.com>
2020-10-28 23:51:46 -0400

    scss edits, health and madness UI, pseudocode as well

--- src/Components/Rooms/Foyer.js
+++ src/Components/Rooms/Foyer.js
@@ -22,18 +22,29 @@ export const Foyer = (prop) => {
 			<h1>Foyer</h1>
 
 			<div className='game-screen'>
+				{/* Game screen is 3 main categories, background, character pane, and text box. */}
 				<div className='background'>
 					<div className='graphic'>
+						{/* Inserting graphic for object/character of situation in question. can be used to show objects, enemies, etc. */}
 						<img src='https://i.imgur.com/pNOztsH.png' />
 					</div>
 				</div>
 				<div className='character-panel'>
 					<div className='character'></div>
 					<div className='health'>
-						<div className='health-icon'></div>
-						<div className='health-number'></div>
+						<div className='health-icon'>
+							{/* This is where we insert the code that calls the current number for health */}
+							<div className='health-number'>100</div>
+						</div>
+						
+						<div className='madness-icon'>
+							{/* This is where we put the code to output the current madness number */}
+							<div className='madness-number'>5</div>
+						</div>
+						
 					</div>
 				</div>
+				{/* This will be the location of everything text base and using buttons */}
 				<div className='text-box'>
 					<p>You enter the foyer of the grand estate. A lone beam of moonlight shines down to the center of the room.</p> <p>Within it's pale glow is the flash of metal from a steel knife. Looks like someone dropped this in a hurry.</p>
 							{hasKnife ? (
