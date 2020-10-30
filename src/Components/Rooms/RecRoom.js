0ec28f31af11614ab7f591dfd3ae6d2eacf43a69
Woodrow Thompson <woodrow.thompson.iii@gmail.com>
2020-10-30 11:46:26 -0400

    boss key

--- src/Components/Rooms/RecRoom.js
+++ src/Components/Rooms/RecRoom.js
@@ -45,7 +45,7 @@ export const RecRoom = (prop) => {
 					<div className='graphic'>
 						{/* Inserting graphic for object/character of situation in question. can be used to show objects, enemies, etc. */}
 						{hasKey ? 
-                            [<img src='https://i.imgur.com/NVvZndA.png' />,
+                            [<img src='https://i.imgur.com/aYuS5ud.png' />,
                             <p>A secret compartment pops open containing a strange key.</p>,
                             <p>You picked up a mysterious looking key...</p>
                             ] : <img src='https://i.imgur.com/pNOztsH.png' />} 
