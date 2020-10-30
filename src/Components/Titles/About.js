368ff4e1bc14ad2c39e3b5f7c99ab1a06a8274c0
Zane <zanebgriffin@gmail.com>
2020-10-30 10:23:17 -0500

    about page style

--- src/Components/Titles/About.js
+++ src/Components/Titles/About.js
@@ -16,17 +16,22 @@ export const About = (props) => {
 
             <h3>Meet the Team</h3>
 
+        <div>
             <h5>Stasys Meclazcke</h5>
             <img src='https://media-exp1.licdn.com/dms/image/C5635AQExDzxXAPF_VA/profile-framedphoto-shrink_200_200/0?e=1604098800&v=beta&t=UeSyfe22oHU7hotIIm_RpffvNgKm9jXhLSh-oAcUyZk' />
             <p>Full stack developer with a passion in creating applications, cars, and games. Prior experience including multiple games on Windows store. Created the front end structure and read/wrote data to/from the backend</p>
-
+        </div>
+        <div>
             <h5>Woodrow Thompson</h5>
             <img src='https://media-exp1.licdn.com/dms/image/C4E03AQGJYRGcfxyTFg/profile-displayphoto-shrink_200_200/0?e=1609372800&v=beta&t=VmiuA1JIERsaTKCkJNewVWWmEfVmogiyDkGAzp0krsQ' />
             <p>Web Designer with multiple years of experience in both digital and traditional arts. Years of experience with Adobe Creative Suite and Graphic Design. Responsible for scripts, providing visual assets, and structuring user interface and layout for gaming elements.</p>
+        </div>
+        <div>
             <h5>Zane Griffin</h5>
             <img src='https://media-exp1.licdn.com/dms/image/C4E03AQFmYaZs6x0PLw/profile-displayphoto-shrink_200_200/0?e=1609372800&v=beta&t=NysmMPJgymAiI6cqlsf7wFkTlYS0bctarbnN9uKuXZs' />
             <p>An aspiring software engineer dedicated to full responsive design. Prior experience is in art and design with Adobe Creative Suite. She was responsible for the backend server and assisting with frontend logic. </p>
             <button onClick={() => handleClick()}>Back</button>
+        </div>
         </div>
     )
 };
