7461a047c8f513e32bb714cb19990381b096b341
Zane <zanebgriffin@gmail.com>
2020-10-30 10:38:36 -0500

    score page styles

--- src/Components/Scoreboard/Scoreboard.js
+++ src/Components/Scoreboard/Scoreboard.js
@@ -9,14 +9,19 @@ export const Scoreboard = (props) => {
 
 	return <div className='scoreboard'>
 		<h1>Highscores</h1>
+		<button onClick={() => handleClick()}>Back</button>
+		<div className='titles'>
+			<h3>player</h3>
+			<h5>score</h5>
+		</div>
 		{props.scoreboard[0].players ? props.scoreboard[0].players.map((player) => {
 			return(
-				<>
+				<div className='scores'>
 					<h3>{player.username}</h3>
 					<h5>{player.score}</h5>
-				</>
+				</div>
 			)
 		}) : ''}
-		<button onClick={() => handleClick()}>Back</button>
+		
 	</div>;
 };
